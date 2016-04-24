1deefee43cefa8719dd702a098c76873b397171c
xuefeng Huang <timhxf@gmail.com>
2016-04-24 15:30:58 +0000

    add model form

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from forms import SignUpForm
 
 # Create your views here.
 def index(req):
-    return render(req, 'index.html', {})
+    form = SignUpForm(req.POST or None)
+    
+    if form.is_valid():
+        full_name = form.cleaned_data.get('full_name')
+        if not full_name:
+            full_name = 'new name'
+        form.full_name = full_name
+        form.save()
+        
+    return render(req, 'index.html', {'form':form})
